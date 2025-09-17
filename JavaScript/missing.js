1913430039affa95e60f233b807814918ed16df3
Yousef Walid <aboelnaga0200824@gmail.com>
2025-09-17 19:26:27 +0300

    Add files via upload

---
+++ JavaScript/missing.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let storePerson = JSON.parse(localStorage.getItem("persons")) || [];
+  let listPerson = document.getElementById("missingList");
+
+  if (!listPerson) return;
+  if (storePerson.length === 0) {
+    listPerson.innerHTML = "<p>لا توجد بلاغات مفقودين حتى الآن.</p>";
+  } else {
+    storePerson.forEach((person) => {
+      let card = document.createElement("div");
+      card.className = "col-md-4";
+      card.innerHTML = `
+        <div class="card missing-card shadow">
+          <img src="${person.img}" loading="lazy" class="preview" alt="صورة ${person.name}">
+          <div class="card-body text-center">
+            <h3 class="card-title fw-bold fs-3">
+            <i class="fas fa-user-circle me-1 text-success"></i>  ${person.name}
+            </h3>
+            <p class="card-text">العمر: ${person.age} سنة</p>
+            <p class="card-text"><i class="fas fa-map-marker-alt me-1 text-muted"></i> المدينة: ${person.governorate}</p>
+            <button class="btn-detail btn btn-outline-success rounded-pill mt-2">تفاصيل أكثر</button>
+            </div>
+            </div>`;
+      // بعد ما تضيف الكرت، أربط الزر
+      let btn = card.querySelector(".btn-detail");
+      btn.addEventListener("click", () => {
+        localStorage.setItem("selectedPerson", JSON.stringify(person));
+        window.location.href = "details.html";
+      });
+
+      // ضيف الكرت للصفحة
+      listPerson.appendChild(card);
+    });
+  }
+});
---
+++ JavaScript/report.js
@@ -0,0 +1,117 @@
+let image = document.getElementById("personImage");
+let preview = "";
+
+// Raed Files For Images
+image.addEventListener("change", () => {
+  let file = image.files[0];
+  if (file) {
+    let reader = new FileReader();
+    reader.onload = function (e) {
+      preview = e.target.result;
+      localStorage.setItem("previewImagePerson", preview);
+    };
+    reader.readAsDataURL(file);
+  }
+});
+
+let buttonMain = document.getElementById("btn-main");
+
+// Valedition Form
+buttonMain.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let namePerson = document.getElementById("personName").value.trim();
+  let agePerson = Number(document.getElementById("age").value.trim());
+  let governorate = document.getElementById("governorateInp").value.trim();
+  let phone = document.getElementById("phoneNumber").value.trim();
+  let lastSeen = document.getElementById("lastSeen").value.trim();
+  let details = document.getElementById("details").value.trim();
+  let reporter = document.getElementById("reporterName").value.trim();
+
+  if (!preview) {
+    preview = localStorage.getItem("previewImagePerson") || "";
+  }
+
+  let fields = [
+    preview,
+    phone,
+    governorate,
+    reporter,
+    details,
+    lastSeen,
+    namePerson,
+  ];
+
+  let empty = fields.some((field) => field === "");
+  if (empty) {
+    Swal.fire({
+      icon: "error",
+      title: "خطأ",
+      text: "أملأ جميع الحقول",
+      confirmButtonColor: "#3085d6",
+      confirmButtonText: "حسنا",
+    });
+    return;
+  }
+
+  if (isNaN(agePerson) || agePerson < 0) {
+    Swal.fire({
+      icon: "error",
+      title: "خطأ",
+      text: "الرجاء إدخال عمر صحيح",
+      confirmButtonColor: "#3085d6",
+      confirmButtonText: "حسنا",
+    });
+    return;
+  }
+
+  if (!/^\d{11}$/.test(phone)) {
+    Swal.fire({
+      icon: "error",
+      title: "خطأ",
+      text: "رقم الهاتف يجب أن يتكون من 11 رقمًا",
+      confirmButtonColor: "#3085d6",
+      confirmButtonText: "حسنا",
+    });
+    return;
+  }
+
+  let reportDate = new Date();
+  localStorage.setItem("reportDate", reportDate.toISOString());
+
+  let person = {
+    img: preview,
+    name: namePerson,
+    age: agePerson,
+    governorate: governorate,
+    phone: phone,
+    lastSeen: lastSeen,
+    details: details,
+    reporter: reporter,
+    reportDate: new Date().toISOString()
+  };
+
+  let persons = JSON.parse(localStorage.getItem("persons")) || [];
+  persons.push(person);
+  localStorage.setItem("persons", JSON.stringify(persons));
+
+  Swal.fire({
+    icon: "success",
+    title: "تم الإضافة بنجاح",
+    text: "نجحت في إضافة البلاغ",
+    confirmButtonText: "حسنا",
+  });
+
+  setTimeout(() => {
+    window.location.href = "missing.html";
+  }, 2000);
+});
+
+function letterOnly(input) {
+  var regex = /[^a-z أ-ي]/gi;
+  input.value = input.value.replace(regex, "");
+}
+function numberOnly(input) {
+  var regex = /[^0-9]/g;
+  input.value = input.value.replace(regex, "");
+}
