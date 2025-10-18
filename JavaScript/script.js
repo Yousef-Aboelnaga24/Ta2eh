f98d4841b22c1f22f040f2045e17b71fea32c425
Yousef Aboelnaga <aboelnaga0200824@gmail.com>
2025-10-18 18:44:19 +0300

    Add files via upload

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -17,21 +17,32 @@ let lastThreePersons = persons.slice(-3).reverse();
 
 lastMissing.innerHTML = "";
 
-lastThreePersons.forEach((person) => {
+lastThreePersons.forEach((person, index) => {
   lastMissing.innerHTML += `
   <div class="col-md-4">
     <div class="card missing-card shadow-sm border-0 h-100 transition">
       <img src="${person.img}" class="card-img-top" alt="شخص مفقود">
-        <div class="card-body text-center">
-          <h5 class="card-title fw-bold fs-4 text-dark">
-            <i class="fas fa-user-circle me-1 text-success"></i> ${person.name}
-          </h5>
-          <p class="card-text"><i class="fas fa-birthday-cake me-1 text-muted"></i> العمر: ${person.age} سنة</p>
-          <p class="card-text"><i class="fas fa-map-marker-alt me-1 text-muted"></i> المدينة: ${person.governorate}</p>
-          <button class="btn-detail btn btn-outline-success rounded-pill mt-2">تفاصيل أكثر</button>
-        </div>
+      <div class="card-body text-center">
+        <h5 class="card-title fw-bold fs-4 text-dark">
+          <i class="fas fa-user-circle me-1 text-success"></i> ${person.name}
+        </h5>
+        <p class="card-text"><i class="fas fa-birthday-cake me-1 text-muted"></i> العمر: ${person.age} سنة</p>
+        <p class="card-text"><i class="fas fa-map-marker-alt me-1 text-muted"></i> المدينة: ${person.governorate}</p>
+        <button class="btn-detail btn btn-outline-success rounded-pill mt-2" data-index="${index}">تفاصيل أكثر</button>
+      </div>
     </div>
   </div>`;
+});
+
+// إضافة حدث لكل زرار
+document.querySelectorAll(".btn-detail").forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    // نجيب الـ index من data attribute
+    let index = e.target.getAttribute("data-index");
+    let person = lastThreePersons[index];
+    localStorage.setItem("selectedPerson", JSON.stringify(person));
+    window.location.href = "./HTML/details.html";
+  });
 });
 
 // document.addEventListener("contextmenu", (e) => {
